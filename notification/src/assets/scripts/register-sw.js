62bfeb2e3cd3d750a0714e612a17a8da12cb10bc
Le Vu Hoang Khanh <lvhkhanh@gmail.com>
2020-07-19 18:37:51 +0700

    initializeUI

--- notification/src/assets/scripts/register-sw.js
+++ notification/src/assets/scripts/register-sw.js
@@ -17,3 +17,18 @@ if ("serviceWorker" in navigator && "PushManager" in window) {
 }
 
 const applicationServerPublicKey = "<Your Public Key>";
+
+function initializeUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription().then(function (subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log("User IS subscribed.");
+    } else {
+      console.log("User is NOT subscribed.");
+    }
+
+    updateBtn();
+  });
+}
